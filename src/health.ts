ccd4c47c59990f97375132dc05307a0706545750
Asteria Moon <asteriasmoons@outlook.com>
2026-02-03 06:30:41 -0600

    try now

--- src/health.ts
+++ src/health.ts
@@ -14,7 +14,7 @@ import eventShareApiRouter from "./miniapp/eventShare-api";
 
 import habitsRouter from "./miniapp/habits";
 
-import checklistRouter from "./checklist-api";
+import checklistRouter from "./checklist";
 
 
 type StartServerOpts = {
